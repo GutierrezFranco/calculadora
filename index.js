a85bd791d62d3b77824facbbfc009da2d0af33aa
GutierrezFranco <111407223+GutierrezFranco@users.noreply.github.com>
2023-06-13 23:27:59 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function calculateRange() {
+  var inputNumber = parseFloat(document.getElementById("inputNumber").value);
+  var inputPercentage = parseFloat(document.getElementById("inputPercentage").value);
+
+  var tolerance = inputNumber * (inputPercentage / 100);
+  var minRange = inputNumber - tolerance;
+  var maxRange = inputNumber + tolerance;
+
+  document.getElementById("output").innerHTML = "Rango mínimo: " + minRange + "<br>Rango máximo: " + maxRange;
+}
